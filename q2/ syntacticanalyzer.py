9bf11d3b47f6b35f2ec93776c519804fbedbf392
titivat <titivatmo@gmail.com>
2021-05-25 05:34:52 +0700

    done q2

--- q2/ syntacticanalyzer.py
+++ q2/ syntacticanalyzer.py
@@ -4,6 +4,11 @@ sys.path.append("..")
 from compilerProject.q1.lexicalAnaylzer import tokens
 from compilerProject.q1.lexicalAnaylzer import lexer
 
+error_status = ""
+variable = {}
+undifineVar = ""
+writeFile = open( "./q2/61090041.tok", 'w')
+
 def p_calc(p):
     '''
     calc : expression
@@ -17,6 +22,18 @@ def p_var_assign(p):
     var_assign : VAR EQU expression
     '''
     p[0] = (p[1], '=', p[3])
+    global variable
+    variable[p[1]] = 1
+
+    if( not str(p[3]).isdigit() ):
+        if( str(p[3]) not in variable ):
+            global undifineVar
+            undifineVar = str(p[3])
+            global error_status
+            error_status = "undefine"
+            result = str( abs(p.lexer.lexpos-2) )
+            raise Exception(result)
+
 
 def p_expression(p):
     '''
@@ -57,6 +74,8 @@ def p_expression_var(p):
 
 def p_error(p):
     result = str(p.lexer.lineno)
+    global error_status
+    error_status = "error"
     raise Exception(result)
 
 def p_empty(p):
@@ -66,22 +85,33 @@ def p_empty(p):
     p[0] = None
 
 def saveTofile(p):
+
     outString = ""
     for item in range( len(p) ):
         outString += str(p[item])
-    print( '(' + outString + ')')
+    global writeFile
+    writeFile.write( '(' + outString + ')' + '\n')
 
 # main
 parser = yacc.yacc()
 path = os.getcwd()
 
-itemList = ["23+8", "25*0", "5NUM^ 3.0","x=5","10*x","x =y","x!=5", "x=(5+9"]
+openFile = open( "./input.txt", 'r')
+body = openFile.read().split("\n")
+openFile.close()
+
+itemList = body
 
 for line in range( len(itemList)):
     try:
         itemList[line].replace(" ", "")
         parser.parse( itemList[line] , lexer = lexer)
     except Exception as e:
-        error = "Error in line " + str(line + 1) + ", pos " + str(e)
-        print(error)
+        if( error_status == "error" ):
+            error = "Error in line " + str(line + 1) + ", pos " + str(e) + '\n'
+        else:
+            error = "Undefined variable " + undifineVar +" at line "+ str(line + 1) + ", pos " + str(e) + "\n"
+        writeFile.write( error )
         continue
+
+print("\n+++++end of syntacticanalyzer++++\n")
