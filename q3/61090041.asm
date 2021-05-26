cd660b954a30b0bcc0823bad7d725d79e951353f
titivat <titivatmo@gmail.com>
2021-05-26 11:05:34 +0700

    add: add to text file

--- q3/61090041.asm
+++ q3/61090041.asm
@@ -1,3 +1,32 @@
+LD R0 #23
+LD R1 #8
+ADD.i R2 R0 R1
+ST @print R2
+
+LD R0 #2.5
+LD R1 #0
+FL.i R1 R1
+MUL.f R2 R0 R1
+ST @print R2
+
 ERROR
+
+LD R0 #5
+ST @x R0
+
+LD R0 #10
+LD R1 @x
+MUL.i R2 R0 R1
+ST @print R2
+
 ERROR
+
+LD R0 @x
+LD R1 #5
+FL.i R0 R0
+FL.i R1 R1
+NE.f R2 R0 R1
+ST @print R2
+
 ERROR
+
--- q3/codegenerator.py
+++ q3/codegenerator.py
@@ -6,8 +6,12 @@ from compilerProject.q1.lexicalAnaylzer import lexer
 from compilerProject.q1.lexicalAnaylzer import tokens
 
 variable = {}
-undifineVar = ""
+register = {}
+stringToFile = ""
 writeFile = open("./q3/61090041.asm", 'w')
+writeFile.close()
+
+writeFile = open("./q3/61090041.asm", 'a')
 
 precedence = (
     ('left', 'GREATER', 'GREATEREQU', 'LESS', 'LESSEUQ', 'EQUEQU', 'NOTEQU'),
@@ -16,33 +20,65 @@ precedence = (
     ('left', 'POW')
 )
 
-def getOperation( operation ):
-    if( operation == "*" ):
+
+def addString(string):
+    global stringToFile
+    stringToFile += string + '\n'
+
+
+def addTofile():
+    global stringToFile
+    global stringToFile
+    writeFile.write(stringToFile + '\n')
+
+
+def addRegister(value):
+    global register
+    register[value] = len(register)
+
+
+def getReister(value):
+    global register
+    return register[value]
+
+
+def calculateValue(trub):
+    for item in trub:
+        if(type(item) == float):
+            return 1.0
+        if(item == "/"):
+            return 1.0
+    return 1
+
+
+def getOperation(operation):
+    if(operation == "*"):
         return "MUL."
-    elif( operation == "+"):
+    elif(operation == "+"):
         return "ADD."
-    elif( operation == "-"):
+    elif(operation == "-"):
         return "SUB."
-    elif( operation == "/" ):
+    elif(operation == "/"):
         return "DIV."
-    elif( operation == "//" ):
+    elif(operation == "//"):
         return "DIV."
-    elif( operation == "==" ):
+    elif(operation == "=="):
         return "EQ.f"
-    elif( operation == "!=" ):
+    elif(operation == "!="):
         return "NE.f"
-    elif( operation == ">"):
+    elif(operation == ">"):
         return "GT.f"
-    elif( operation == ">="):
+    elif(operation == ">="):
         return "GE.f"
-    elif( operation == "<"):
+    elif(operation == "<"):
         return "LT.f"
-    elif( operation == "<="):
+    elif(operation == "<="):
         return "LE.f"
-    elif( operation ==  "=="):
+    elif(operation == "=="):
         return "EQ.f"
-    elif( operation == "!=" ):
+    elif(operation == "!="):
         return "NE.f"
+
 
 def p_calc(p):
     '''
@@ -50,9 +86,21 @@ def p_calc(p):
          | var_assign
          | empty
     '''
-    print("p_calc")
-    if((p[1][1]) != "=" ):
-        print(f"ST @print R{'2'}")
+    global register
+    global variable
+    global stringToFile
+    if(len(p[1]) <= 1):
+        addString(f"ST @print R{len(register)-1}")
+        addTofile()
+        stringToFile = ""
+
+    elif((p[1][1]) != "="):
+        addString(f"ST @print R{len(register)-1}")
+        addTofile()
+        stringToFile = ""
+
+    register = {}
+
 
 def p_var_assign(p):
     '''
@@ -60,20 +108,18 @@ def p_var_assign(p):
     '''
     p[0] = (p[1], '=', p[3])
     global variable
-    variable[p[1]] = 1
 
     new_varable = str(p[3])
     if(not new_varable.isdigit()):
         if(new_varable not in variable):
-            global undifineVar
-            undifineVar = new_varable
             result = str(abs(p.lexer.lexpos-2))
             raise Exception(result)
 
-    print("p_var_assign")
-    print(p[1], p[3])
-    print(f"ST @{p[1]} R{'1'}")
     variable[p[1]] = p[3]
+
+    addString(f"ST @{p[1]} R{getReister(p[3])}" + '\n')
+    variable[p[1]] = p[3]
+
 
 def p_expression(p):
     '''
@@ -91,22 +137,50 @@ def p_expression(p):
                | expression NOTEQU expression
     '''
     p[0] = (p[1], p[2], p[3])
-    ##
-    print('p_expression:')
+    temp = p[1]
+    if(type(p[1]) == tuple):
+        p[1] = calculateValue(p[1])
+
     operation = getOperation(p[2])
-    if( p[2] in ["==",'!=','<','<=','>','>=']):
-        #check here later
-        print(f"{operation} R{'1'} R{'2'} R{'3'}")
-    elif( isinstance(p[1], int) and isinstance(p[3], int)):
-        print(f"{operation}i R{'1'} R{'2'} R{'3'}")
-    elif(isinstance(p[1], int)):
-        print(f"FL.i R{'1'} R{'1'}")
-        print(f"{operation}f R{'1'} R{'2'} R{'3'}")
-    elif( isinstance(p[3], int)):
-        print(f"FL.i R{'1'} R{'1'}")
-        print(f"{operation}f R{'1'} R{'2'} R{'3'}")
+    global variable
+    global register
+    exp1 = p[1]
+    exp2 = p[3]
+    reg1 = getReister(p[1])
+    reg2 = getReister(p[3])
+    addRegister((temp, p[3]))
+    newReg = getReister((temp, p[3]))
+    newReg = len(register) - 1
+
+    if((type(exp1) == str)):
+        exp1 = variable[exp1]
+
+    if(type(exp2) == str):
+        exp2 = variable[exp2]
+
+    if(p[2] in ["==", '!=', '<', '<=', '>', '>=']):
+        if(isinstance(exp1, int)):
+            addString(f"FL.i R{reg1} R{reg1}")
+
+        if(isinstance(exp2, int)):
+            addString(f"FL.i R{reg2} R{reg2}")
+
+        addString(f"{operation} R{newReg} R{reg1} R{reg2}")
+
+    elif(isinstance(exp1, int) and isinstance(exp2, int)):
+        addString(f"{operation}i R{newReg} R{reg1} R{reg2}")
+
+    elif(isinstance(exp1, int)):
+        addString(f"FL.i R{reg1} R{reg1}")
+        addString(f"{operation}f R{newReg} R{reg1} R{reg2}")
+
+    elif(isinstance(exp2, int)):
+        addString(f"FL.i R{'1'} R{'1'}")
+        addString(f"{operation}f R{newReg} R{reg1} R{reg2}")
+
     else:
-        print(f"{operation}f R{'1'} R{'2'} R{'3'}")
+        addString(f"{operation}f R{newReg} R{reg1} R{reg2}")
+
 
 def p_expression_int_float_sci(p):
     '''
@@ -116,9 +190,10 @@ def p_expression_int_float_sci(p):
                | parentheses
     '''
     p[0] = p[1]
-    print('p_expression_int_float_sci:')
-    var = "1"
-    print(f"LD R{var} #{p[1]}")
+
+    if(type(p[1]) != tuple):
+        addRegister(p[1])
+        addString(f"LD R{getReister(p[1])} #{p[1]}")
 
 
 def p_parentheses(p):
@@ -133,8 +208,11 @@ def p_expression_var(p):
     expression : VAR
     '''
     p[0] = (p[1])
-    print('p_expression_var:')
-    print(f"LD R{'1'} @{p[1]}")
+    pValue = p[1]
+    global variable
+    if(p[1] in variable):
+        addRegister(pValue)
+        addString(f"LD R{getReister(pValue)} @{pValue}")
 
 
 def p_error(p):
@@ -150,6 +228,7 @@ def p_empty(p):
     '''
     p[0] = None
 
+
 # main
 parser = yacc.yacc()
 path = os.getcwd()
@@ -163,13 +242,12 @@ itemList = body
 for line in range(len(itemList)):
     try:
         itemList[line].replace(" ", "")
-        print( itemList[line] + "\n")
         parser.parse(itemList[line], lexer=lexer)
-        print("++++++++++++++++++++++++++++")
     except Exception as e:
-        error = "ERROR\n"
+        register = {}
+        error = "ERROR\n\n"
         writeFile.write(error)
-        print("++++++++++++Error+++++++++++++")
         continue
 
+writeFile.close()
 print("\n+++++end of condegenerator++++\n")
